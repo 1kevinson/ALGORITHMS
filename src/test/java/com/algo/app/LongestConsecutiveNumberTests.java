6cdc480eccaa9c14e56e5f67c6a023e507120be0
Arsene Kevin <kevinkouomeu@icloud.com>
2022-01-10 01:34:43 +0100

    👷 - setup sonarqube

--- src/test/java/com/algo/app/LongestConsecutiveNumberTests.java
+++ src/test/java/com/algo/app/LongestConsecutiveNumberTests.java
@@ -1,5 +1,6 @@
 package com.algo.app;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LongestConsecutiveNumberTests {
 
     @Test
+    @DisplayName("Should return 4")
     public void shouldReturn4() {
         // ARRANGE
         final int[] numbersArray = new int[]{ 100, 4, 200, 1, 3, 2 };
@@ -20,6 +22,7 @@ class LongestConsecutiveNumberTests {
     }
 
     @Test
+    @DisplayName("Should return 9")
     public void shoudReturn9() {
         // ARRANGE
         final int[] numbersArray = new int[]{ 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
@@ -34,6 +37,7 @@ class LongestConsecutiveNumberTests {
     }
 
     @Test
+    @DisplayName("Should return 0")
     public void shoudReturn0() {
         // ARRANGE
         final int[] numbersArray = new int[]{};
